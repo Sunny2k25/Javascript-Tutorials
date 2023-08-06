8f82233ea8ce8dcf002033f5cd6b527fefbc868e
Sunny Jaiswal <117598761+Sunny2k25@users.noreply.github.com>
2023-08-06 19:55:59 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+
+//alert("Hello") //we are using nodejs, not browser
+
+console.log(3+3);
+
+
+let name = "Sunny"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefind => 
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof undefined); //undefind
+console.log(typeof null); //object
