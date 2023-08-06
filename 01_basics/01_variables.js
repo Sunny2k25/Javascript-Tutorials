448580a684b591acaab0b1afe0dee0bb1dbb2957
Sunny Jaiswal <117598761+Sunny2k25@users.noreply.github.com>
2023-08-06 19:35:41 +0000

    Variables complete

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 14453;
+let accountEmail = "sunny@gmail.com";
+var accountPassword ="12345";
+accountCity = "Bihar";
+let accountState ;
+
+
+// accountId = 15652;
+accountEmail = "sunnyjaiswal@gmail.com";
+accountPassword = "14325";
+accountCity = "Bengluru";
+
+
+/*
+  Prefer not to use var
+  because of issue in block scope and function
+
+ */
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
