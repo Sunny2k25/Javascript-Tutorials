942d6423999fcee748da9bdade72d5dfcf8e909e
Sunny Jaiswal <117598761+Sunny2k25@users.noreply.github.com>
2023-08-08 19:14:23 +0000

    Datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,53 @@
+//primitive data types
+//call by reference
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+//how to declare Symbol
+// const Id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(Id == anotherId)
+
+const bigNumber = 3456789101212232n
+
+
+//Javascript is dynamically typed language
+
+
+
+// Reference type(Non primitive)
+
+// Array, Objects, Functions
+
+
+// Array
+const heros = ["shaktiman", "naagraj", "doga"]
+
+//Objects
+let myObj = {
+    name : "Sunny",
+    age : 20,
+}
+
+//Functions
+const myFunction =function () {
+    console.log("hello world");
+}
+
+console.log(typeof anotherId);
+
+//typeof val
+// If val is undefined, return "undefined".
+// 5. If val is null, return "object".
+// 6. If val is a String, return "string".
+// 7. If val is a Symbol, return "symbol".
+// 8. If val is a Boolean, return "boolean".
+// 9. If val is a Number, return "number".
+// 10. If val is a BigInt, return "bigint".
