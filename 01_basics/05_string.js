52d2203f8811faf46bb60c742619ddfff1fd2157
Sunny Jaiswal <117598761+Sunny2k25@users.noreply.github.com>
2023-08-13 03:19:12 +0000

    nums and nums

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -37,3 +37,21 @@ console.log(url.replace('%20','-'));
 console.log(url.includes('sunder'));
 
 console.log(gameName.split('-'));
+
+
+
+/* IMPORTANT THINGS TO REMEMBER
+-> Backtest use karna hain 
+*/
+//at function
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+let index = 5;
+
+console.log(`Using an index of ${index} the character returned is ${sentence.at(index)}`);
+// Expected output: "Using an index of 5 the character returned is u"
+
+index = -4;
+
+console.log(`Using an index of ${index} the character returned is ${sentence.at(index)}`);
+// Expected output: "Using an index of -4 the character returned is d"
---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,38 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// console.log(typeof balance);
+
+
+const otherNumber = 1123.8966;
+//Decimal ke pehle priority mil rahi hain
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// *************** MATHS *****************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6))
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(4, 3, 2, 1));
+// console.log(Math.max(4, 3, 2, 1));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random())*10 + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max-min + 1)) + min)
