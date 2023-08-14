022f15da52a3c7b9807af115363428954d0bc1a3
Sunny Jaiswal <117598761+Sunny2k25@users.noreply.github.com>
2023-08-14 16:14:36 +0000

    Array complete

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -6,7 +6,7 @@ const myHeros = ["shktiman", "Ben 10"]
 
 const myArr2 = new Array(1, 2, 3, 4)
 
-  
+    
 //console.log(myArr[1]);
 
 
@@ -15,28 +15,31 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.push(6);
 // myArr.push(7);
 // myArr.pop();
-// myArr.unshift(9);
-// myArr.shift();
+//myArr.unshift(9);
+//myArr.shift();
 
-// console.log(myArr.includes(9));
+//console.log(myArr.includes(9));
 // console.log(myArr.indexOf(9));
 
 const newArr = myArr.join()
 
-// console.log(myArr);
+console.log(myArr);
 // console.log(typeof newArr);
 // console.log(newArr);
 
 //slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
-const myn1 = myArr.slice(1,3);
+// const myn1 = myArr.slice(1,3);
 
-console.log((myn1));
+// console.log((myn1));
 
-console.log("B ", myArr);
+// console.log("B ", myArr);
 
-const myn2 = myArr.splice(1, 3);
-console.log("C", myArr);
-console.log(myn2);
+// const myn2 = myArr.splice(1, 3);
+// console.log("C", myArr);
+// console.log(myn2);
+
+
+
