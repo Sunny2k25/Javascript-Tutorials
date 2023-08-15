ade406692fdfbc7a7ac45ddb9392012eca8a9e15
Sunny Jaiswal <117598761+Sunny2k25@users.noreply.github.com>
2023-08-15 13:31:21 +0000

    Scope complete

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -11,10 +11,55 @@ if(true){
     let a = 10
     const b = 20
     // var c = 30
-    console.log("Inner value of a:", a);
+    //console.log("Inner value of a:", a);
 
 }
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+function one(){
+    const username = "Sunny"
+
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+
+//one()
+
+
+if (true) {
+    const username = "Sunny"
+    if(username === "Sunny")
+    {
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+
+//console.log(username);
+
+
+// ++++++++++++++ Intresting +++++++++++++
+
+// console.log(addone(5));
+function addone(num){
+    return num+1;
+
+
+}
+
+
+
+const addTwo = function(num){
+    return num+2;
+}
+addTwo(5)
