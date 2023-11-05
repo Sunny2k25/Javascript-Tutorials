c673014d7e5ee2b55482258b836d51414311245d
Sunny Jaiswal <117598761+Sunny2k25@users.noreply.github.com>
2023-11-05 07:54:45 +0000

    prototype

---
+++ 10_classes_and_Oops/prototype.js
@@ -0,0 +1,32 @@
+// let myName = "hitesh"
+// let mychannel = "Chai"
+
+
+// console.log(myName.truelength);
+// console.log(myName.truelength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.hitesh = function(){
+    console.log(`hitesh is present in all objects`);
+}
+
+Array.prototype.heyHitesh = function(){
+    console.log(`Hitesh says hello`);
+}
+
+// heroPower.hitesh()
+// myHeros.hitesh()
+// myHeros.heyHitesh()
+// heroPower.heyHitesh()
